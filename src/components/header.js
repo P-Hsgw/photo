4f6e9a7c6b00a2d87ee4c3417167367bb53bf206
P-Hsgw <contact@p-hsgw.com>
2021-05-12 14:02:30 +0900

    Add basic responsive menu
    Still need to create proper modal menu for mobile

--- src/components/header.js
+++ src/components/header.js
@@ -6,26 +6,26 @@ import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
   <div className="absolute z-50 inset-x-0 top-2">
-      <ul className="flex items-center justify-center h-10 space-x-8 text-2xl uppercase">
+      <ul className="flex items-center justify-center h-10 space-x-8 text-4xl uppercase">
         <li>
           {" "}
           <Link to="/" className="text-gray-50">
             Home
           </Link>
         </li>
-        <li>
+        <li className="md:list-item hidden">
           {" "}
-          <Link to="/street" className="text-gray-50">
+          <Link to="/street" className="text-gray-50 ">
             Street
           </Link>
         </li>
-        <li>
+        <li className="md:list-item hidden ">
           {" "}
           <Link to="/portrait" className="text-gray-50">
             Portrait
           </Link>
         </li>
-        <li>
+        <li className="md:list-item hidden">
           {" "}
           <Link to="/nature" className="text-gray-50">
             Nature
