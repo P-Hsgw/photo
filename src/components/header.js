896818ecc7a0ef97b60e277edbde7c69058a18b9
P-Hsgw <contact@p-hsgw.com>
2021-05-13 14:10:03 +0900

    Fix position of invisible, responsive div

--- src/components/header.js
+++ src/components/header.js
@@ -9,40 +9,51 @@ const MobileHeader = () => {
     setClicked(!clicked)
   }
 
+  const unclick = () => {
+    setClicked(false)
+  }
+
   return (
-    <div
-      className={`md:hidden z-20 h-screen ${clicked ? "block" : "none"}`}
-      onClick={isClicked}
-    >
-      <ul className="z-30 md:hidden flex flex-col items-center justify-center space-y-5 text-4xl uppercase drop-shadow-2xl text-gray-50">
-        <li
-          onClick={isClicked}
-          className={`cursor-pointer ${clicked ? "underline" : "no-underline"}`}
-        >
-          Menu
-        </li>
-        <li className={`${clicked ? "list-item" : "hidden"}`}>
-          <Link to="/" className="text-gray-50">
-            Home
-          </Link>
-        </li>
-        <li className={`${clicked ? "list-item" : "hidden"}`}>
-          <Link to="/street" className="text-gray-50 ">
-            Street
-          </Link>
-        </li>
-        <li className={`${clicked ? "list-item" : "hidden"}`}>
-          <Link to="/portrait" className="text-gray-50">
-            Portrait
-          </Link>
-        </li>
-        <li className={`${clicked ? "list-item" : "hidden"}`}>
-          <Link to="/nature" className="text-gray-50">
-            Nature
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div
+        className={`absolute inset-0 md:hidden z-10 h-screen ${
+          clicked ? "block" : "hidden"
+        } bee`}
+        onClick={isClicked}
+       />
+      <div className="z-40 absolute inset-x-0 top-0">
+        <ul className="md:hidden flex flex-col items-center justify-center space-y-5 text-4xl uppercase drop-shadow-2xl text-gray-50">
+          <li
+            onClick={isClicked}
+            className={`cursor-pointer ${
+              clicked ? "underline" : "no-underline"
+            } pt-4`}
+          >
+            Menu
+          </li>
+          <li className={`${clicked ? "list-item" : "hidden"}`}>
+            <Link to="/" className="text-gray-50 z-40">
+              Home
+            </Link>
+          </li>
+          <li className={`${clicked ? "list-item" : "hidden"}`}>
+            <Link to="/street" className="text-gray-50 ">
+              Street
+            </Link>
+          </li>
+          <li className={`${clicked ? "list-item" : "hidden"}`}>
+            <Link to="/portrait" className="text-gray-50">
+              Portrait
+            </Link>
+          </li>
+          <li className={`${clicked ? "list-item" : "hidden"}`}>
+            <Link to="/nature" className="text-gray-50">
+              Nature
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
 
