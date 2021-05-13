51dae4faa82092d1bc487fa5123289db57c81d00
P-Hsgw <contact@p-hsgw.com>
2021-05-13 23:29:54 +0900

    Add padding in the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
   const getCurrentYEar = () => {
     return new Date().getFullYear();
   }
-  
+
   return (
-  <div className="w-screen h-5 text-center text-gray-50 mb-1">
+  <div className="w-screen h-5 text-center text-gray-50 mb-1 pb-8">
     © 2019-{getCurrentYEar()} Tsukinode.  All rights reserved.
   </div>)
 }
