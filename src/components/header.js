d520b45efd97de5803eb50f3a7eb12e6b3972e0e
P-Hsgw <contact@p-hsgw.com>
2021-05-13 13:37:28 +0900

    Fix a bug with scrollable page

--- src/components/header.js
+++ src/components/header.js
@@ -10,45 +10,47 @@ const MobileHeader = () => {
   }
 
   return (
-
-    <div className={`md:hidden h-screen z-20 ${clicked? "block" : "none"}`} onClick={isClicked}>
-    <ul className="z-30 md:hidden flex flex-col items-center justify-center space-y-5 text-4xl uppercase drop-shadow-2xl text-gray-50">
-      <li onClick={isClicked} className={`cursor-pointer ${clicked ? "underline" : "no-underline"}`}>
-        Menu
-      </li>
-      <li className={`${clicked? "list-item" : "hidden"}`}>
-      <Link to="/" className="text-gray-50">
-          Home
-        </Link>
-      </li>
-      <li className={`${clicked? "list-item" : "hidden"}`}>
-      <Link to="/street" className="text-gray-50 ">
-          Street
-        </Link>
-      </li>
-      <li className={`${clicked? "list-item" : "hidden"}`}>
-      <Link to="/portrait" className="text-gray-50">
-          Portrait
-        </Link>
-      </li>
-      <li className={`${clicked? "list-item" : "hidden"}`}>
-      <Link to="/nature" className="text-gray-50">
-          Nature
-        </Link>
-      </li>
-    </ul>
-    
-
+    <div
+      className={`md:hidden z-20 h-screen ${clicked ? "block" : "none"}`}
+      onClick={isClicked}
+    >
+      <ul className="z-30 md:hidden flex flex-col items-center justify-center space-y-5 text-4xl uppercase drop-shadow-2xl text-gray-50">
+        <li
+          onClick={isClicked}
+          className={`cursor-pointer ${clicked ? "underline" : "no-underline"}`}
+        >
+          Menu
+        </li>
+        <li className={`${clicked ? "list-item" : "hidden"}`}>
+          <Link to="/" className="text-gray-50">
+            Home
+          </Link>
+        </li>
+        <li className={`${clicked ? "list-item" : "hidden"}`}>
+          <Link to="/street" className="text-gray-50 ">
+            Street
+          </Link>
+        </li>
+        <li className={`${clicked ? "list-item" : "hidden"}`}>
+          <Link to="/portrait" className="text-gray-50">
+            Portrait
+          </Link>
+        </li>
+        <li className={`${clicked ? "list-item" : "hidden"}`}>
+          <Link to="/nature" className="text-gray-50">
+            Nature
+          </Link>
+        </li>
+      </ul>
     </div>
-
   )
 }
 
 const Header = ({ siteTitle }) => (
-  <div className="absolute z-50 inset-x-0 top-4 ">
-      <MobileHeader />
+  <div className="absolute z-50 inset-x-0 top-0">
+    <MobileHeader />
 
-    <ul className="hidden md:flex items-center justify-center h-10 space-x-8 text-4xl uppercase drop-shadow-2xl">
+    <ul className="hidden md:flex items-center justify-center h-10 space-x-8 text-4xl uppercase drop-shadow-2xl mt-4">
       <li className="md:list-item hidden">
         {" "}
         <Link to="/" className="text-gray-50">
