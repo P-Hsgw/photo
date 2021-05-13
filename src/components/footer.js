343a91dd17e1f1e440fe468f54be7694fdff09a5
P-Hsgw <contact@p-hsgw.com>
2021-05-13 23:28:33 +0900

    Add getCurrentYear function to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,14 @@
 import * as React from "react"
 
-const Footer = () => (
+const Footer = () => {
+  const getCurrentYEar = () => {
+    return new Date().getFullYear();
+  }
+  
+  return (
   <div className="w-screen h-5 text-center text-gray-50 mb-1">
-    © 2019-2021 Tsukinode.  All rights reserved.
-  </div>
-)
+    © 2019-{getCurrentYEar()} Tsukinode.  All rights reserved.
+  </div>)
+}
 
 export default Footer
